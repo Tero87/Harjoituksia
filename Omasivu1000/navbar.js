35dc038bc5f0626771b379230cc226b25734f263
Tero87 <131463003+Tero87@users.noreply.github.com>
2023-08-09 20:30:41 +0300

    Delete Omasivu1000 directory

--- Omasivu1000/navbar.js
+++
@@ -1,8 +0,0 @@
-function NavbarFunction() {
-    var x = document.getElementById("navb");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
